e0fc68b2c5b3d9c69b7c2e87bfcf19c6ec331435
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-15 15:55:13 -0400

    api functional

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`${newNote} added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = notes;
