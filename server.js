98602226725f5fa62dbbc3d071a0650a5b51b170
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-15 15:59:59 -0400

    server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(clog);
+app.use(express.json());
+app.use('/api', api);
+
+// link static pages
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
